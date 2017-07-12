085b3bba66c316af56326e5a4ca660b7d33fa4ed
Derek Garnett <drkgrntt@gmail.com>
2017-07-11 20:38:13 -0500

    add start script

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,6 @@ router.get("/results", middleware.isLoggedIn, function(req, res) {
     var query = req.query.search;
     var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
     request(url, function(error, response, body) {
-        // I STILL NEED TO REDIRECT ERRORS
         if(!error && response.statusCode == 200) {
             var data = JSON.parse(body);
             res.render("results", {data: data});
